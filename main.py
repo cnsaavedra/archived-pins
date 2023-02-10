edc515cf8711df819a754042edc459d1faa14855
Christian Saavedra <christian-saavedra@hotmail.com>
2023-02-10 14:49:05 -0500

    first

--- main.py
+++ main.py
@@ -10,16 +10,39 @@ client = discord.Client()
 # client.events are for reading inputs from user in discord, and for bot to return a msg
 @client.event
 async def on_ready():
-    print("bello".format(client))
+    print("BELLO! I'm ready to archive your pins (Hosted from a local computer).".format(client))
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!create_archived_pins'):
-        await message.channel.send('Please choose a channel to archive the pins.')
+        # Check if the author has the necessary permissions to perform the action
+        if not message.author.permissions_in(message.channel).manage_channels:
+            await message.channel.send('You do not have the necessary permissions to perform this action.')
+            return
 
+        # Ask the user to select/type an existing channel name
+        await message.channel.send('Please type the name of the channel you want to archive pins from:')
+        response = await client.wait_for('message', check=lambda msg: msg.author == message.author)
+        channel_name = response.content
 
-# ALL COMMANDS UNDERNEATH, ABOVE IS HOW TO CALL THEM
+        # Check if the selected channel exists
+        channel = discord.utils.get(message.guild.channels, name=channel_name)
+        if not channel:
+            await message.channel.send(f'The channel "{channel_name}" does not exist on this server.')
+            return
+
+        # Create the new archived_pins channel
+        archived_pins_channel_name = f'archived_pins_{channel_name}'
+        archived_pins_channel = await message.guild.create_text_channel(archived_pins_channel_name)
+
+        # Copy the existing pins from the selected channel to the new archived_pins channel
+        pins = await channel.pins()
+        for pin in pins:
+            await archived_pins_channel.send(pin.content)
+
+        await message.channel.send(f'The pins from "{channel_name}" have been archived to "{archived_pins_channel_name}".')
+
 
 # runs the client using secret token from our env file
 client.run(os.getenv('BOT_TOKEN'))
